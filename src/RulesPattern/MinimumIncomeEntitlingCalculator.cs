4e2eed4593cf3d0ce2f85ee72d9b4911b5eb61da
Andrea Castellano <a.castellano@visualsoft-consulting.com>
2019-04-07 12:52:32 +0200

    Add folder for the rules

--- src/RulesPattern/MinimumIncomeEntitlingCalculator.cs
+++ src/RulesPattern/MinimumIncomeEntitlingCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using RulesPattern.Rules;
 
 namespace RulesPattern
 {
--- src/RulesPattern/Rules/IEntitledRule.cs
+++ src/RulesPattern/Rules/IEntitledRule.cs
@@ -1,4 +1,4 @@
-﻿namespace RulesPattern
+﻿namespace RulesPattern.Rules
 {
     public interface IEntitledRule
     {
